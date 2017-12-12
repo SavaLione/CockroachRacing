34f4c0e137dbf56b63b544094993a72d2e220bb9
SavaLione <monologuesplus@gmail.com>
2017-12-12 15:32:36 +0500

    fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <Windows.h>
+
 using namespace std;
+
 void v_menu_main();
+
 int main() {
 	SetConsoleTitle("Cockroach_Racing");
 	system("cls");
 	v_menu_main();
 	return 0;
 }
-//SavaLione
